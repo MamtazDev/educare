30eb57774ad4333e56348420522788d396524680
tonni-akter-dev <tanniakhter123@gmail.com>
2023-12-01 18:27:25 +0600

    featured course complete

---
+++ js/BlogSlider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#owl-demo").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        navText: ["Previous", "Next"], // Font Awesome icons
+        autoplay: false,
+        dots: true, 
+    });
+});
