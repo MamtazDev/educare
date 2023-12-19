643fc18e04d5e44461739ba8c91cfe815e6b22dc
tonni-akter-dev <tanniakhter123@gmail.com>
2023-12-19 17:14:38 +0600

    home 3 page design done

---
+++ js/testimonialSlider.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("#owl-demo").owlCarousel({
+        items: 1,
+        loop: true, 
+        nav: true, 
+        navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"], // Font Awesome icons
+        autoplay: false,
+        dots: false,
+    });
+});
+
+
+
