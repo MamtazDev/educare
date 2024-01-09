65193cf2badb5709648ab29e40e9768bea407bc5
muhammadashiqurrahman <getintouch.ashiq@gmail.com>
2024-01-09 14:58:17 +0600

    :: index update

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  // WOW JS INIT
+  $(function () {
+    var wow = new WOW({
+      animateClass: "animated",
+      mobile: true,
+    });
+    wow.init();
+  });
+});
